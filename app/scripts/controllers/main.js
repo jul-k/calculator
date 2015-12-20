b465133bcec7f8c0f5cfd07ea38f8b1e62960843
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2015-12-20 17:49:50 +0200

    Create mvp

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,5 +11,26 @@
 var app = angular.module('calculatorApp');
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.expressionState = "";
+    $scope.lastInput = "";
+    $scope.calculated = false;
 
+    $scope.record = function(a) {
+        if($scope.calculated===true) {
+            $scope.expressionState = a;
+            $scope.calculated = false;
+        } else {
+            $scope.expressionState += a;
+        }
+    }
+
+    $scope.result = function() {
+        $scope.expressionState = eval($scope.expressionState);
+        $scope.calculated = true;
+    }
+
+    $scope.clear = function() {
+        $scope.expressionState = "";
+        $scope.calculated = false;
+    }
 }]);
